7377e837a5568dae2795cd97846e1ca26f4e1814
shankar akashkore <shankarakashkore32@gmail.com>
2026-04-28 21:19:13 +0530

    Generate page layout for title

--- client/reactjs/src/pages/Generate.tsx
+++ client/reactjs/src/pages/Generate.tsx
@@ -25,6 +25,39 @@ export const Generate = () => {
                 <h2 className="text-xl font-bold text-zinc-100 mb-1">Create Your Thumbnail</h2>
                 <p className="text-sm text-zinc-400">Describe your vision and let AI bring it to life</p>
               </div>
+
+              <div className="space-y-5">
+                <div className="space-y-2">
+                  <label className="block text-sm font-medium">Title or Topic</label>
+
+                  <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} maxLength={100} placeholder="e.g., 10 Tips for Better Sleep" className="w-full px-4 py-3 rounded-lg border border-white/12 bg-black/20 text-zinc-100 placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-pink-500"/>
+
+                  <div className="flex justify-end">
+                    <span className="text-xs text-zinc-400">{title.length}/100</span>
+                  </div>
+                </div>
+
+                {/* RatioSelector */}
+                {/* StyleSelector */}
+                {/* ColorSelector */}
+
+                {/* Details */}
+                <div className="space-y-2">
+                  <label className="block text-sm font-medium">
+                    Additional Prompts <span className="text-zinc-400 text-xs">(Optional)</span>
+                  </label>
+
+                  <textarea value={additionalDetails} onChange={(e) => setAdditionalDetails(e.target.value)} rows={3} placeholder="Add any specific elements, mood or style preferences..." className="w-full px-4 py-3 rounded-lg border border-white/10 bg-white/6 text-zinc-100 placeholder:text-zinc-400 focus:outline-none focus:ring-2 focus:ring-pink-500 resize-none"/>
+
+                </div>
+              </div>
+
+              {/* Button */}
+              {!id && (
+                <button className="text-[15px] w-full py-3.5 rounded-xl font-medium bg-linear-to-b from-pink-500 to-pink-600 hover:from-pink-700 disabled:cursor-not-allowed transition-colors">
+                  {loading ? "Generating..." : "Generate Thumbnail"}
+                </button>
+              )}
             </div>
           </div>
 
