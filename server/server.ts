48fe70e2783280ade521918b4184c877b179eec1
shankar akashkore <shankarakashkore32@gmail.com>
2026-07-10 21:26:17 +0530

    Updated cors

--- server/server.ts
+++ server/server.ts
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', "https://thumbforge-zeta.vercel.app"],
     credentials: true
 }))
 
